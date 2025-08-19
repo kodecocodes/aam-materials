4a67defd7510343a3f7fb409c92861800a39368e
Zahidur Rahman Faisal <zr.faisal@programmer.net>
2025-08-20 01:14:19 +1000

    code: Added Text Recognition

--- 04-on-device-intelligence-with-mlkit/projects/final/app/src/main/java/com/kodeco/android/aam/MainViewModel.kt
+++ 04-on-device-intelligence-with-mlkit/projects/final/app/src/main/java/com/kodeco/android/aam/MainViewModel.kt
@@ -1,13 +1,20 @@
 package com.kodeco.android.aam
 
+import android.app.Application
 import android.util.Log
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.application
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.common.InputImage.fromFilePath
 import com.google.mlkit.vision.documentscanner.GmsDocumentScanner
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_JPEG
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.SCANNER_MODE_FULL
 import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
 import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import java.io.IOException
 
 /**
  * ViewModel for the main screen.
@@ -16,12 +23,12 @@ import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
  * to the UI. The list of breeds is initialized when the ViewModel
  * is created.
  */
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
   // 1: Prepare Document Scanning Client
   fun prepareScanner(): GmsDocumentScanner {
     val options = GmsDocumentScannerOptions.Builder()
-      .setPageLimit(3)
+      .setPageLimit(1)
       .setResultFormats(RESULT_FORMAT_JPEG)
       .setScannerMode(SCANNER_MODE_FULL)
       .build()
@@ -31,9 +38,30 @@ class MainViewModel : ViewModel() {
   fun extractTextFromResult(scanResult: GmsDocumentScanningResult?) {
     scanResult?.pages?.let { pages ->
       for (page in pages) {
-//              val imageUri = pages[0].imageUri
         Log.d("scannerLauncher", "Selected page URI: $page")
+        try {
+          val image = fromFilePath(application, page.imageUri)
+          applyTextRecognition(image) { extractedText ->
+            Log.d("scannerLauncher", "Extracted text: $extractedText")
+          }
+        } catch (e: IOException) {
+          Log.e("scannerLauncher", "ERROR: ${e.printStackTrace()}")
+        }
       }
     }
+  }
+
+  fun applyTextRecognition(image: InputImage, onCompleted: (String?) -> Unit) {
+    TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+      .process(image)
+      .addOnSuccessListener { visionText ->
+        val resultText = visionText.text
+        Log.d("scannerLauncher", "Extracted text: $resultText")
+        onCompleted(resultText)
+      }
+      .addOnFailureListener { e ->
+        Log.e("scannerLauncher", "Extracted text ERROR: ${e.printStackTrace()}")
+        onCompleted(null)
+      }
   }
 }
